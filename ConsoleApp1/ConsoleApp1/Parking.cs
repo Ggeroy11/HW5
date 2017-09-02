89f2422114d44a0369ccf989b76c407aadedc120
Ggeroy11 <elnar1995@mail.ru>
2017-09-02 14:55:58 +0600

    это не домашка

---
+++ ConsoleApp1/ConsoleApp1/Parking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Parking
+    {
+        Car[] car;
+        public Parking() {
+            car = new Car[] { new Car("Toyota"), new Car("Chevrolet"), new Car("Lamborghini") };
+        }
+
+    }
+}
